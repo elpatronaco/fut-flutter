b5cde300216405c10a328e40a7e4d3fee492bd53
Pau Colomé Diaz <38383754+MuerteBolonesa@users.noreply.github.com>
2020-11-10 20:29:26 +0100

    Retoc

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'FUT Viewer',
       initialRoute: '/',
-      routes: {'/': (BuildContext context) => PlayerCard(testData)},
+      routes: {'/': (BuildContext context) => HomePage()},
     );
   }
 }
